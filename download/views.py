16ba61867701b7b0a69cfd0c5d901a48a98fa5e5
Dave Page <dpage@pgadmin.org>
2017-05-05 11:14:03 +0100

    Show alternate downloads on download pages, and more format improvements.

--- download/views.py
+++ download/views.py
@@ -20,6 +20,9 @@ def index(request):
 
 def download_list(request, slug):
     distribution = get_object_or_404(Distribution, slug=slug, active=True)
+    distributions = Distribution.objects.filter(package=distribution.package, active=True)
     downloads = Download.objects.filter(distribution=distribution, active=True)
 
-    return render_to_response('download/distribution.html', {'distribution': distribution, 'downloads': downloads})
+    return render_to_response('download/distribution.html', {'distribution': distribution,
+                                                             'distributions': distributions,
+                                                             'downloads': downloads})
