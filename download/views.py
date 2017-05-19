bb483942b280dd749b84d1b810dd783a83e9125b
Dave Page <dpage@pgadmin.org>
2017-05-19 11:50:13 +0100

    Only show downloads for active versions.

--- download/views.py
+++ download/views.py
@@ -21,7 +21,7 @@ def index(request):
 def download_list(request, slug):
     distribution = get_object_or_404(Distribution, slug=slug, active=True)
     distributions = Distribution.objects.filter(package=distribution.package, active=True).exclude(id=distribution.id)
-    downloads = Download.objects.filter(distribution=distribution, active=True)
+    downloads = Download.objects.filter(distribution=distribution, active=True, version__active=True)
 
     return render_to_response('download/distribution.html', {'distribution': distribution,
                                                              'distributions': distributions,
