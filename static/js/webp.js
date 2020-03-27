02b7cddc3d7a69bd7ba84d1ec022bbe704a090a8
Dave Page <dpage@pgadmin.org>
2020-03-27 14:29:07 +0000

    Use webp images where possible.

---
+++ static/js/webp.js
@@ -0,0 +1,16 @@
+async function supportsWebp() {
+  if (!self.createImageBitmap) return false;
+
+  const webpData = 'data:image/webp;base64,UklGRh4AAABXRUJQVlA4TBEAAAAvAAAAAAfQ//73v/+BiOh/AAA=';
+  const blob = await fetch(webpData).then(r => r.blob());
+  return createImageBitmap(blob).then(() => true, () => false);
+}
+
+(async () => {
+  if(await supportsWebp()) {
+    $("html").addClass("webp");
+  }
+  else {
+    $("html").addClass("no-webp");
+  }
+})();
