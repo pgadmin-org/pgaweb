78f7b2f7538883311daae851761c360c35fe317b
Dave Page <dpage@pgadmin.org>
2023-07-04 14:44:33 +0100

    Need to use createrepo_c now.

--- tools/rebuild-yum-repo.sh
+++ tools/rebuild-yum-repo.sh
@@ -9,10 +9,10 @@ DISTRO_FAMILY=$1
 DISTRO_NAME=$2
 DISTRO_VERSION=$3
 
-/usr/bin/createrepo /var/ftp/pgadmin4/yum/${DISTRO_FAMILY}/${DISTRO_NAME}-${DISTRO_VERSION}-x86_64
+/usr/bin/createrepo_c /var/ftp/pgadmin4/yum/${DISTRO_FAMILY}/${DISTRO_NAME}-${DISTRO_VERSION}-x86_64
 /usr/bin/gpg --yes --detach-sign --armor /var/ftp/pgadmin4/yum/${DISTRO_FAMILY}/${DISTRO_NAME}-${DISTRO_VERSION}-x86_64/repodata/repomd.xml
 
-cd /var/ftp/pgadmin4/yum/${DISTRO_FAMILY}/ 
+cd /var/ftp/pgadmin4/yum/${DISTRO_FAMILY}/
 ln -snf ${DISTRO_NAME}-${DISTRO_VERSION}-x86_64 ${DISTRO_NAME}-${DISTRO_VERSION}Server-x86_64
 ln -snf ${DISTRO_NAME}-${DISTRO_VERSION}-x86_64 ${DISTRO_NAME}-${DISTRO_VERSION}Workstation-x86_64
 ln -snf ${DISTRO_NAME}-${DISTRO_VERSION}-x86_64 ${DISTRO_NAME}-${DISTRO_VERSION}Client-x86_64
