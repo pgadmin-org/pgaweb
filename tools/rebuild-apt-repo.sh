6158199e13586ab0fb2ef62fc146c7f81daee26b
Dave Page <dpage@pgadmin.org>
2023-07-04 15:00:55 +0100

    Exit if there is an error.

--- tools/rebuild-apt-repo.sh
+++ tools/rebuild-apt-repo.sh
@@ -7,22 +7,22 @@ fi
 
 DISTRO_CODENAME=$1
 
-for ARCH in all amd64 i386; do 
+for ARCH in all amd64 i386; do
     mkdir -p /var/ftp/pgadmin4/apt/${DISTRO_CODENAME}/dists/pgadmin4/main/binary-${ARCH}
 done
 
-for ARCH in all amd64 i386; do 
+for ARCH in all amd64 i386; do
     cd /var/ftp/pgadmin4/apt/${DISTRO_CODENAME}
-    apt-ftparchive packages -c=/home/pgaupload/aptftp.conf dists/pgadmin4/main/binary-${ARCH}  > dists/pgadmin4/main/binary-${ARCH}/Packages
-    gzip -f -k dists/pgadmin4/main/binary-${ARCH}/Packages; 
+    apt-ftparchive packages -c=/home/pgaupload/aptftp.conf dists/pgadmin4/main/binary-${ARCH}  > dists/pgadmin4/main/binary-${ARCH}/Packages || exit 1
+    gzip -f -k dists/pgadmin4/main/binary-${ARCH}/Packages || exit 1
 done
 
 cd /var/ftp/pgadmin4/apt/${DISTRO_CODENAME}/dists/pgadmin4
-apt-ftparchive release -c=/home/pgaupload/aptftp.conf . > Release && gzip -f -k Release
+apt-ftparchive release -c=/home/pgaupload/aptftp.conf . > Release && gzip -f -k Release || exit 1
 
 rm -f Release.gpg
-gpg -u packages@pgadmin.org -bao Release.gpg Release
+gpg -u packages@pgadmin.org -bao Release.gpg Release || exit 1
 
 rm -f InRelease
-gpg -u packages@pgadmin.org --clear-sign --output InRelease Release
+gpg -u packages@pgadmin.org --clear-sign --output InRelease Release || exit 1
 
